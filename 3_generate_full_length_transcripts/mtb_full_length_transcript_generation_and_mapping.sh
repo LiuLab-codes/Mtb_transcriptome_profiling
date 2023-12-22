15cad0e03311d1c59656ed80a5aee73f7a5897f4
LiuLab-codes <41641367+LiuLab-codes@users.noreply.github.com>
2023-12-22 10:29:43 -0500

    Add files via upload

---
+++ 3_generate_full_length_transcripts/mtb_full_length_transcript_generation_and_mapping.sh
@@ -0,0 +1,65 @@
+set -ue
+
+
+#input file is *.sam file, however you can skip the first several lines if you start with bam file type.
+#Ensure you have installed samtools, bedtools, and set the right directorly for the reference genome "NC_018143_TB_lambda_genome" and rRNA reference informaiton "TB_H37RV.RNA.bed".
+#Output file *PE_2_S.bowtie2mapping.sorted.bam is the most important file for the downstream data analysis.
+
+
+
+sam_file=$1
+#name=$2
+
+filename_1=$(basename "$sam_file")
+
+filename_1="${filename_1%%.sam}"
+
+samtools view -Sb  $sam_file >$filename_1.bam
+samtools sort $filename_1.bam > $filename_1.sorted.bam
+samtools index $filename_1.sorted.bam
+
+
+samtools sort -n $filename_1.sorted.bam  -o $filename_1.sorted_n.bam
+
+samtools view -bf 0x2 $filename_1.sorted_n.bam | bedtools bamtobed -i stdin -bedpe > $filename_1\_PE.bedpe
+cut -f 1,2,6,7,8,9 $filename_1\_PE.bedpe > $filename_1\_PE_2.bedpe
+rm $filename_1\_PE.bedpe
+bedtools getfasta -fi  ~/ref_genome/NC_018143_TB_lambda_genome.fna    -bed $filename_1\_PE_2.bedpe  -s  -name  > $filename_1\_PE_2_S.fa
+
+bowtie2 -p 10   -f --very-sensitive-local    --ff -x ~/ref_genome/NC_018143_TB_lambda_genome  $filename_1\_PE_2_S.fa -S $filename_1\_PE_2_S.bowtie2mapping.sam
+
+cat $filename_1\_PE_2_S.bowtie2mapping.sam  |cut -f 6 |sed "s/M//g" |sed "s/\*$//g"| grep -vE "^-" | grep -vE "^0" >$filename_1\_total_mapping.txt
+samtools view -Sb $filename_1\_PE_2_S.bowtie2mapping.sam >$filename_1\_PE_2_S.bowtie2mapping.bam
+samtools sort $filename_1\_PE_2_S.bowtie2mapping.bam > $filename_1\_PE_2_S.bowtie2mapping.sorted.bam
+samtools index $filename_1\_PE_2_S.bowtie2mapping.sorted.bam 
+
+
+bedtools intersect -abam  $filename_1\_PE_2_S.bowtie2mapping.sorted.bam -b  ~/ref_genome/TB_H37RV.RNA.bed  -v > $filename_1.rRNA_filtered.bam
+samtools index $filename_1.rRNA_filtered.bam
+samtools view  $filename_1.rRNA_filtered.bam > $filename_1.rRNA_filtered.sam
+cat $filename_1.rRNA_filtered.sam   |cut -f 6 | sed "s/M//g" |sed "s/\*$//g"| grep -vE "^-" | grep -vE "^0" >$filename_1.RNA_filtered_length.txt
+
+
+rm $filename_1.rRNA_filtered.sam 
+
+samtools sort $filename_1.rRNA_filtered.bam > $filename_1.rRNA_filtered_sorted.bam
+samtools index $filename_1.rRNA_filtered_sorted.bam
+
+
+samtools view -L ~/ref_genome/TB_H37RV.RNA.bed   $filename_1\_PE_2_S.bowtie2mapping.sorted.bam |cut -f 6 |sed "s/M//g" | grep -vE "^-" | grep -vE "^0" > $filename_1.rRNA_length.txt
+
+file="txt"
+if [ -d "$file" ]
+then
+	echo "$file exists and the new files will be added into txt file."
+else
+	mkdir txt
+fi
+
+mv $filename_1*.txt txt/
+
+
+rm  $filename_1\_PE_2_S.bowtie2mapping.sam $filename_1.bam $filename_1.sorted.bam $filename_1.sorted.bam.bai $filename_1\_PE_2_S.bowtie2mapping.bam 
+
+
+
